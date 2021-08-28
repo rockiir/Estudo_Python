23b554ce730c96ea0ea59a7e375246f8d902824a
rockiir <70121094+rockiir@users.noreply.github.com>
2021-08-28 03:17:11 -0300

    Basico

---
+++ Basico/Comentarios/Comentarios.py
@@ -0,0 +1,7 @@
+#isto é um comentário
+print('Linha 1')  #comentario
+
+""" comentário 
+multi
+linhas
+"""
---
+++ Basico/print.py
@@ -0,0 +1,8 @@
+print('luiz' , 'João' , 'Ana')
+print('luiz' , 'João' , 'Ana')
+print('luiz' , 'João' , 'Ana', sep= '_', end='')  # End não pula linha, e Sep substitui a separação
+print('luiz' , 'João' , 'Ana', sep= '_')
+
+
+print('824' , '176' , '070', sep= '.', end = '_')
+print('18')
