ead8cb006ef10f92aea3cccd7bcda799056e31a4
rockiir <70121094+rockiir@users.noreply.github.com>
2021-08-29 02:33:15 -0300

    basico

---
+++ Basico/inElifElse/ifElifElse.py
@@ -0,0 +1,27 @@
+# If, Elif e Else
+
+if False:
+    print('false')
+elif True:
+    print('verdadeiro')
+elif True:
+    print('verdadeiro')
+else:
+    print('nao')
+
+
+"""
+Operadores Relacionais
+== > >= < <= !=
+"""
+num= 3
+if num == 2:
+    print('é igual')
+else:
+    print('não é')
+
+"""
+Operadores lógicos
+and, or, not(inversor)
+in e not in
+"""
