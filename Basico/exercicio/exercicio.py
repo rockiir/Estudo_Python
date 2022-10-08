c79b4330d25979969d524e5566b255435a7e9a8b
rockiir <70121094+rockiir@users.noreply.github.com>
2022-10-08 01:23:00 -0300

    POO

---
+++ Basico/exercicio/exercicio.py
@@ -0,0 +1,15 @@
+"""
+1 - Crie uma função que exibe uma saudação com os parâmetros saudacao e nome.
+"""
+nome = input('Digete o seu nome ')
+print ('nome', nome )
+
+num_1 = input('Digite outro numero: ')
+num_2 = input('Digite outro numero: ')
+num_3 = input('Digite outro numero: ')
+
+operador = input('Digite um operador:')
+Sair = input('sair [S]Sim [N]Nao')
+
+
+
---
+++ POO/main.py
@@ -0,0 +1,26 @@
+from pessoa import Pessoa
+
+p1 = Pessoa('Luiz', 29)
+p2 = Pessoa('Joana', 32)
+
+p1.comer('maça')
+p2.comer('banana')
+p1.parar_comer()
+p1.parar_comer()
+p1.comer('abobobra')
+p1.falar('POO')
+p1.parar_comer()
+p1.falar('POO')
+p1.comer('alimento')
+p1.parar_falar()
+p1.falar('assunto')
+
+print(p1.get_ano_nascimento())
+print(p1.ano_atual)
+
+
+
+
+
+
+
